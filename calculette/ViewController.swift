edcb22512abee28e91ece151bebc1979d4639172
Mathilde Arconte <signup@marconte.fr>
2018-11-26 16:56:33 +0100

    La fillette

--- calculette/ViewController.swift
+++ calculette/ViewController.swift
@@ -11,9 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     
-    
-    @IBOutlet weak var TextField: UILabel!
-    
+    @IBOutlet weak var Label: UILabel!
     
     
     override func viewDidLoad() {
@@ -21,18 +19,69 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    var digit : String = ""
+    var currentDigit : Double = 0.0
+    var previousDigit : Double = 0.0
+    var result :Double = 0.0
+    var operation : String = ""
+    var isTypingNumber = false
+    
+    
+    // Update le digit
     @IBAction func btnClicked(_ sender: UIButton)
     {
-        
-        digit += String((sender.titleLabel?.text)!)
-        
-            print(digit)
-        TextField.text = "Coucou"
-       
-        
+        currentDigit = (currentDigit * 10) + Double((sender.titleLabel?.text)!)!
+        Label.text = String(currentDigit)
+        print(currentDigit)
     }
     
+    // Mettre la calculette à zéro
+    @IBAction func btnInit(_ sender: Any) {
+        Label.text = ""
+        currentDigit = 0
+    }
     
+    // Operation
+    
+    @IBAction func add(_ sender: Any)
+    {
+        previousDigit += currentDigit
+        currentDigit = 0
+        operation = "Addition"
+        print(operation)
+    }
+    
+    // Soustraction
+    
+    @IBAction func sub(_ sender: Any)
+    {
+        previousDigit += currentDigit
+        currentDigit = 0
+        operation = "Soustraction"
+        print(operation)
+    }
+    
+    // Multiplication
+    
+    @IBAction func multi(_ sender: Any)
+    {
+        previousDigit += currentDigit
+        currentDigit = 0
+        operation = "Multiplication"
+        print(operation)
+    }
+    
+    //Resultat
+    @IBAction func result(_ sender: Any) {
+        if (operation == "Addition"){
+            result = previousDigit + currentDigit
+        }
+        if (operation == "Soustraction"){
+            result = previousDigit - currentDigit
+        }
+        if (operation == "Multiplication"){
+            result = previousDigit * currentDigit
+        }
+       Label.text = String(result)
+    }
 }
 
