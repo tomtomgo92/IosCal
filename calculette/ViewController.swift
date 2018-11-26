1a88c11a673698c541d933ec0501534e70504a59
Thomas Millet <tm.lepoleswebforce3@gmail.com>
2018-11-26 13:45:28 +0100

    init

--- calculette/ViewController.swift
+++ calculette/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var btm1: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
